2590ab8a9321b8cd8ecc1b4da9d1806304926685
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-11 15:11:33 +0100

    Initializing the project and implementing the node class here it's called Spot with all the initial methods

---
+++ Astar.py
@@ -0,0 +1,7 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* path finding Algo by cRYP70N")
