8818c69990f2430d87d24b6ead68333d742dccbf
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-11 16:12:33 +0100

    WHAT THE FUCK nothing works

--- Astar.py
+++ Astar.py
@@ -18,117 +18,125 @@ GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
 class Spot:
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-        self.x = row * width
-        self.y = col * width
-        self.color = WHITE
-        self.neighbors = []
-        self.width = width
-        self.total_rows = total_rows
+	def __init__(self, row, col, width, total_rows):
+		self.row = row
+		self.col = col
+		self.x = row * width
+		self.y = col * width
+		self.color = WHITE
+		self.neighbors = []
+		self.width = width
+		self.total_rows = total_rows
 
-    def get_pos(self):
-        return self.row, self.col
+	def get_pos(self):
+		return self.row, self.col
 
-    def is_closed(self):
-        return self.color == RED
+	def is_closed(self):
+		return self.color == RED
 
-    def is_open(self):
-        return self.color == GREEN
+	def is_open(self):
+		return self.color == GREEN
 
-    def is_barrier(self):
-        return self.color == BLACK
+	def is_barrier(self):
+		return self.color == BLACK
 
-    def is_start(self):
-        return self.color == ORANGE
+	def is_start(self):
+		return self.color == ORANGE
 
-    def is_end(self):
-        return self.color == TURQUOISE
+	def is_end(self):
+		return self.color == TURQUOISE
 
-    def reset(self):
-        self.color == WHITE
+	def reset(self):
+		self.color = WHITE
 
 	def make_start(self):
 		self.color = ORANGE
 
-    def make_close(self):
-        self.color = RED
+	def make_close(self):
+		self.color = RED
 
-    def make_open(self):
-        self.color = GREEN
+	def make_open(self):
+		self.color = GREEN
 
-    def make_barrier(self):
-        self.color = BLACK
+	def make_barrier(self):
+		self.color = BLACK
 
-    def make_end(self):
-        self.color = TURQUOISE
+	def make_end(self):
+		self.color = TURQUOISE
 
-    def make_path(self):
-        self.color = PURPPLE
+	def make_path(self):
+		self.color = PURPPLE
 
-    def draw(self, WIN):
-        pygame.draw.rect(WIN, self.color, (self.x, self.y, self.width, self.width))
+	def draw(self, win):
+		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
-    def update_neighbors(self, grid):
-        pass
+	def update_neighbors(self, grid):
+		pass
 
-    def __lt__(self, other):
-        return False
+	def __lt__(self, other):
+		return False
+
 
 def h(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    p2 = (1, 9)
-    return abs(x1 - x2) + abs(y1, y2)
+	x1, y1 = p1
+	x2, y2 = p2
+
+	return abs(x1 - x2) + abs(y1 - y2)
+
 
 def make_grid(rows, width):
-    grid = []
-    gap = width // rows
-    for i in range(raws):
-        grid.append([])
-        for j in range(rows):
-            spot = Spot(i, j, gap, rows)
-            grid[i].append(spot)
+	grid = []
+	gap = width // rows
+	for i in range(rows):
+		grid.append([])
+		for j in range(rows):
+			spot = Spot(i, j, gap, rows)
+			grid[i].append(spot)
 
-    return grid
+	return grid
+
 
 def draw_grid(win, rows, width):
-    gap = width // rows
-    for i in range(rowa):
-        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
-        for j in range(rows):
-            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+	gap = width // rows
+	for i in range(rows):
+		pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+		for j in range(rows):
+			pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
 
 def draw(win, grid, rows, width):
-    win.fill(WHITE)
+	win.fill(WHITE)
 
-    for row in grid:
-        for spot in row:
-            spot.draw(win)
+	for row in grid:
+		for spot in row:
+			spot.draw(win)
 
-    draw_grid(win, rows, width)
-    pygame.display.update()
+	draw_grid(win, rows, width)
+	pygame.display.update()
+
 
 def get_clicked_pos(pos, rows, width):
-    gap = width // rows
-    x, y = pos
+	gap = width // rows
+	y, x = pos
 
-    row = y // gap
-    col = x // gap
-    return row, col
+	row = y // gap
+	col = x // gap
+
+	return row, col
+
 
 def main(win, width):
-    ROWS = 50
-    grid = make_grid(ROWS, width)
+	ROWS = 50
+	grid = make_grid(ROWS, width)
 
-    start = None
-    end = None
+	start = None
+	end = None
 
-    run = True
-    started = False
-    while run:
-        for event in pygame.event.get():
+	run = True
+	started = False
+	while run:
+		draw(win, grid, ROWS, width)
+		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
 			
@@ -137,7 +145,7 @@ def main(win, width):
 
 			if pygame.mouse.get_pressed()[0]: # left mouse button
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, rows, width)
+				row, col = get_clicked_pos(pos, ROWS, width)
 				spot = grid[row][col]
 				if not start:
 					start = spot
@@ -152,5 +160,7 @@ def main(win, width):
 
 			elif pygame.mouse.get_pressed()[2]: # right button
 				pass
+
 	pygame.quit()
-main()
+
+main(WIN, WIDTH)
