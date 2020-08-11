6c3ea4cc952dc6654d8f7fbf05eb1930733ede00
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-11 17:36:01 +0100

    M working on the algorithm everything seems good no problems till now

--- Astar.py
+++ Astar.py
@@ -103,7 +103,29 @@ def algorithm(draw, grid, start, end):
 	g_score[start] = 0
 	f_score = { spot: float("inf") for row in grid for spot in row }
 	f_score[start] = h(start.get_pos(), end_pos())
-	
+
+	open_set_hash = { start }
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			pass # make the path
+			return True
+		
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current
+				g_score[neighbor] = temp_g_score
+				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+
 
 
 def make_grid(rows, width):
